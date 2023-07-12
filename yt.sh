22c00575af1da3127ebec43e10b4b6e71ca0cb31
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2023-07-12 19:25:28 +0200

    feat: add youtube dl script and zshr config

---
+++ yt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo enter path:
+read path
+# youtube-dl -x --audio-format mp3 --rm-cache-dir --verbose --restrict-filenames -o 'Music/youtube-dl/%(title)s.%(ext)s' $path
+
+# https://stackoverflow.com/questions/75495800/error-unable-to-extract-uploader-id-youtube-discord-py
+# TODO switch pack to youtube-dl, need to update first
+
+yt-dlp -x --audio-format mp3 --rm-cache-dir --verbose --restrict-filenames -o 'Music/youtube-dl/%(title)s.%(ext)s' $path
